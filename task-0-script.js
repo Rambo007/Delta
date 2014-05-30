77d46c58faf26857e884f4162c2ef9f876ef0a02
Ramadevi <rama.gksr.aus@gmail.com>
2014-05-30 23:54:57 +0530

    Didnt understand Javascript and the mistakes in it so used jQuery

--- task-0-script.js
+++ task-0-script.js
@@ -1,23 +1,23 @@
-onload = start;
-function start(){
-	round = new animation("ball");
-	round.circle(30,180,-180,150,50);
-}
-function circle(radius,angle1,angle2,steps,interval){
-	var x = document.getElementById("ball").left;
-	var y = document.getElementById("ball").top;
-	var x0 = document.getElementById("box").left;
-	var y0 = document.getElementById("box").top;
+var t = 0;
+
+function circle(){
+	t +=0.05;
+
+	var radius = 30;
+ 	var x0 = document.getElementById('#box').left;
+	var y0 = document.getElementbyId('#box').top;
 	var cenx = x0 / 2;
 	var ceny = y0 / 2;
-	var diff = angle2 - angle1;
- 	var ang = diff/steps;
-	var ar = new Array();
-	for(var i=0;i<steps;i++){
-		angle1+=ang;
-		x = cenx + radius * Math.cos(angle1 * Math.PI/180);
-		y = ceny - radius * Math.sin(angle1 * Math.PI/180);
-	}
-	this.path = ar;
- 	this.animation(interval);
-	}
+ 	var newx = Math.floor(cenx + (radius * Math.cos(t)));
+	var newy = Math.floor(ceny + (radius * Math.sin(t)));
+	$('#ball').animate({
+		top = newy;
+		left: newx;
+	},1,function(){
+		circle();
+	});
+}
+
+$(document).ready(function(){
+	circle();
+});
