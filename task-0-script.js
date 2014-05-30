35bf619580a31eb4fcc9e2228b62d576eae491fc
Ramadevi <rama.gksr.aus@gmail.com>
2014-05-31 00:14:19 +0530

    Last commit was wrong..bleh

--- task-0-script.js
+++ task-0-script.js
@@ -1,23 +1,27 @@
 var t = 0;
 
-function circle(){
-	t +=0.05;
+function circle() {
+    t += 0.05;
 
-	var radius = 30;
- 	var x0 = document.getElementById('#box').left;
-	var y0 = document.getElementbyId('#box').top;
-	var cenx = x0 / 2;
-	var ceny = y0 / 2;
- 	var newx = Math.floor(cenx + (radius * Math.cos(t)));
-	var newy = Math.floor(ceny + (radius * Math.sin(t)));
-	$('#ball').animate({
-		top = newy;
-		left: newx;
-	},1,function(){
-		circle();
-	});
+    var r = document.getElementById('box').left; 
+    var radius = r/2        // radius
+    var x = document.getElementById('box').left;   
+    var xcen =x / 2   // center X position
+    var y = document.getElementById('box').top;   
+    var ycen = y / 2  // center Y position
+
+    var newx = Math.floor(xcen + (r * Math.cos(t)));
+    var newy = Math.floor(ycen + (r * Math.sin(t)));
+
+    $('#box').animate({
+        top: newx,
+        left: newy,
+    }, 1, function() {
+        circle();
+    });
 }
 
-$(document).ready(function(){
-	circle();
-});
+$(document).ready(function() {
+    circle();
+});?
+	
